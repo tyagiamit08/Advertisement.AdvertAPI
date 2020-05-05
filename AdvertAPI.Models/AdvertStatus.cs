e235bb1b6284e100aa90c76f0cac514d58718a59
amit tyagi <amit.tyagi@external.mckinsey.com>
2020-05-05 16:02:35 +0530

    Added .Net Core web Api project and class library project for models

---
+++ AdvertAPI.Models/AdvertStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertAPI.Models
+{
+	public enum AdvertStatus
+	{
+		Pending = 1,
+		Active = 2
+	}
+}
---
+++ AdvertAPI.Models/ConfirmAdvertModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertAPI.Models
+{
+	public class ConfirmAdvertModel
+	{
+		public string Id { get; set; }
+		public AdvertStatus Status { get; set; }
+	}
+}
