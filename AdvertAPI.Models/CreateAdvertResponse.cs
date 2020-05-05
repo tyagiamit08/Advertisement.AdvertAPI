b3dcac8df0e246dceed6ce88f6668c525cd80942
amit tyagi <amit.tyagi@external.mckinsey.com>
2020-05-05 19:55:52 +0530

    Added Advert API Controller with action methods for Create and Confirm

---
+++ AdvertAPI.Models/CreateAdvertResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AdvertAPI.Models
+{
+	public class CreateAdvertResponse
+	{
+		public string Id { get; set; }
+
+	}
+}
---
+++ AdvertAPI/Controllers/AdvertController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvertAPI.Models;
+using AdvertAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertAPI.Controllers
+{
+	[ApiController]
+	[Route("[api/v1/adverts]")]
+	public class AdvertController : ControllerBase
+	{
+		private readonly IAdvertStorageService _advertStorageService;
+
+		public AdvertController(IAdvertStorageService advertStorageService)
+		{
+			_advertStorageService = advertStorageService;
+		}
+
+		[HttpPost]
+		[Route("Create")]
+		[ProducesResponseType(400)]
+		[ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+
+		public async Task<IActionResult> Create(AdvertModel model)
+		{
+			string recordId;
+			try
+			{
+				recordId = await _advertStorageService.Add(model);
+			}
+			catch (KeyNotFoundException)
+			{
+				return new NotFoundResult();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message);
+			}
+
+			return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+		}
+
+		[HttpPut]
+		[Route("Confirm")]
+		[ProducesResponseType(404)]
+		[ProducesResponseType(200)]
+
+		public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+		{
+			try
+			{
+				await _advertStorageService.Confirm(model);
+			}
+			catch (KeyNotFoundException)
+			{
+				return new NotFoundResult();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex.Message);
+			}
+
+			return new OkResult();
+		}
+	}
+}
